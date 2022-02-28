4456f2cc136a708d154208402b12733f84ef933c
Tanoshima <tan04.ma@gmail.com>
2022-02-28 23:55:20 +0900

    update

--- leafret/src/App.js
+++ leafret/src/App.js
@@ -1,10 +1,12 @@
+import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const position = [35.3622222, 138.7313889]
 
 function PopupExample() {
   return (
-    <MapContainer center={position} zoom={13}>
+    // heightの指定がないとレイアウトが崩れる
+    <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
 
       <TileLayer
         attribution="<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
